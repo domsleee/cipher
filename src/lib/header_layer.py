40d8be0ac3ba1a125d2c48da8fbd994361dfa606
Dom Slee <domslee97@gmail.com>
2018-02-24 16:17:01 +1100

    Remove sha256 from header layer

--- src/lib/header_layer.py
+++ src/lib/header_layer.py
@@ -2,7 +2,7 @@ from src.lib.connection import Connection
 from hashlib import sha256
 import logging
 import re
-logger = logging.getLogger('cipher')
+logger = logging.getLogger('header_layer')
 
 class HeaderLayer(Connection):
     def __init__(self, aes_filename):
@@ -12,19 +12,14 @@ class HeaderLayer(Connection):
         self.aes_filename = aes_filename
 
     def _encode(self, child_data=None):
-        sha = sha256(child_data).hexdigest().encode('UTF-8')
-        header = b'{sha256:' + sha + b',aes_filename:' + self.aes_filename + b'}'
+        header = b'{aes_filename:' + self.aes_filename + b'}'
         return header + child_data
 
     def _decode(self, parent_data=None):
-        re_header = re.compile(r'{sha256:([^,]*),aes_filename:([^}]*)}(.*)')
+        re_header = re.compile(r'{aes_filename:([^}]*)}(.*)')
         m = re_header.match(parent_data.decode('UTF-8'))
         if not m:
             raise ValueError('Incorrect format type')
-        sha = m.group(1)
-        aes_filename = m.group(2).encode('UTF-8')
-        data = m.group(3).encode('UTF-8')
-        actual_sha = sha256(data).hexdigest()
-        if sha != actual_sha:
-            raise ValueError('Hash does not match %s vs %s' % (sha, actual_sha))
-        return {'data': data, 'aes_filename': aes_filename}
+        aes_filename = m.group(1).encode('UTF-8')
+        data = m.group(2).encode('UTF-8')
+        return data
--- test/test_header_layer.py
+++ test/test_header_layer.py
@@ -1,4 +1,3 @@
-from hashlib import sha256
 from src.lib.header_layer import HeaderLayer
 import pytest
 
@@ -8,8 +7,7 @@ class TestHeaderLayer:
         self.header_layer = HeaderLayer(self.aes_filename)
 
     def _get_encoded_str(self, sha_data, data):
-        sha = sha256(sha_data).hexdigest().encode('UTF-8')
-        return b'{sha256:'+sha+b',aes_filename:'+self.aes_filename+b'}'+data
+        return b'{aes_filename:'+self.aes_filename+b'}'+data
 
     def test_constructor_enforces_bytestring(self):
         with pytest.raises(ValueError):
@@ -24,10 +22,4 @@ class TestHeaderLayer:
         data = b'abc'
         encoded = self._get_encoded_str(data, data)
         res = self.header_layer.do_decode(encoded)
-        assert(res == {'data': data, 'aes_filename': self.aes_filename})
-
-    def test_decode_with_wrong_hash(self):
-        data = b'abc'
-        encoded = self._get_encoded_str(data+b'123', data)
-        with pytest.raises(ValueError):
-            self.header_layer.do_decode(encoded)
+        assert(res == data)
